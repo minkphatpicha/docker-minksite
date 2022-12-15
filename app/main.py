f31d44d7a063f9e4b87a6dfb1598570f52111eb9
IamMink <118274875+minkphatpicha@users.noreply.github.com>
2022-12-15 14:44:45 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ import streamlit as st
 import page1
 import page2
 import page3
+import page4
 
 PAGES = {
     'Introduction': page1,
     'CV': page2,
-    'Page 3': page3
+    'Page 3': page3,
+    'Page 4': page4
 }
 
 st.sidebar.title('Hello World!')
